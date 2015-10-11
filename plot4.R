8fd5c9ad6e97ee4eb4ec63fb6542b3a5842543e8
BLNRGH <blenoir@southfaceventures.com>
2015-10-11 21:50:49 +0100

    added comment

--- plot4.R
+++ plot4.R
@@ -5,6 +5,8 @@ data <- fread(input = 'grep -e "^[1-2]/2/2007" "household_power_consumption.txt"
 
 # set column names
 setnames(data, c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# set datetime 
 datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
 # write to png
